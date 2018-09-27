b4b51d492499d289f16c4c55f1febddd37c19e94
Doug Gregor <dgregor@apple.com>
2018-09-27 15:56:18 -0700

    [IRGen] Mark calls to swift_getAssociatedTypeWitness() nounwind/readnone.
    swift_getAssociatedTypeWitness() is logically readnone because it is the
    only function that accesses associated type witness information within the
    witness table. Mark calls to it as readnone and nounwind.

--- lib/IRGen/GenProto.cpp
+++ lib/IRGen/GenProto.cpp
@@ -3564,7 +3564,8 @@ irgen::emitAssociatedTypeMetadataRef(IRGenFunction &IGF,
                                        wtable,
                                        parentMetadata,
                                        assocTypeDescriptor });
-
+  call->setDoesNotThrow();
+  call->setDoesNotAccessMemory();
   return MetadataResponse::handle(IGF, request, call);
 }
 
